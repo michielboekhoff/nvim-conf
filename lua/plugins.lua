bea76f8fe8e25ac7f2c893f4848d5a8d9bc56eec
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-04-27 16:30:07 +0100

    Remove old lua plugins file

--- lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-return require('packer').startup(function(use)
-  use 
-  use 
-
-  use 'ray-x/guihua.lua' -- recommended if need floating window support
-  use 'neovim/nvim-lspconfig'
-  use 
-
-end)
